7c5ea6b1149443fd9c0bab1940ed8fe7e5a43c26
ColtonJudy <coltonjudy16@gmail.com>
2021-09-27 16:43:57 -0500

    Adding Object class

--- JavaPractice1/Main.java
+++ JavaPractice1/Main.java
@@ -3,6 +3,7 @@ package JavaPractice1;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        Object object = new Object();
+        object.testMethod();
     }
 }
---
+++ JavaPractice1/Object.java
@@ -0,0 +1,12 @@
+package JavaPractice1;
+
+public class Object {
+    
+    public Object() {
+        System.out.println("Object Constructor");
+    }
+
+    public void testMethod() {
+        System.out.println("Running testMethod");
+    }
+}
