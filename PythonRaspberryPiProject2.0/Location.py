1961b9adf7b6f03e0edcca8e3a647fc07f34d1cf
ColtonJudy <73195148+ColtonJudy@users.noreply.github.com>
2021-11-11 11:04:35 -0600

    Organized code & implemented location finding

--- PythonRaspberryPiProject2.0/Location.py
+++ PythonRaspberryPiProject2.0/Location.py
@@ -1,1 +1,14 @@
+from geopy.geocoders import Nominatim
+
+#Find the nearest city to the given coordinates using Nominatim API
+def find_nearest_city(lat, lon):
+    geolocator = Nominatim(user_agent="Colton's Weather Raspberry Pi Project")
+
+    coords = (lat, lon)
+    location = geolocator.reverse(coords)
+
+    return location
+
 #TODO implement receiving GPS data via PI dongle
+def getLocation():
+    return 'null'
--- PythonRaspberryPiProject2.0/Main.py
+++ PythonRaspberryPiProject2.0/Main.py
@@ -1,27 +1,47 @@
-from tkinter.constants import TOP
+from tkinter.constants import BOTTOM, TOP
 from Weather import Weather
 import tkinter as tk
 import tkinter.ttk as ttk
 
-weather = Weather(36.533221, -87.349698)
+lat = 35.155826
+lon = -90.051063
 
 root = tk.Tk()
 root.geometry('800x600')
 root.title('Colton\'s Weather Program')
 
-location_label = tk.Label(root, text = "Location: " + weather.info['location'])
+#TKinter labels
+location_label = tk.Label(root)
 location_label.pack(side=TOP)
 
-lat_label = tk.Label(root, text = "LAT: " + str(weather.settings['lat']))
+lat_label = tk.Label(root)
 lat_label.pack(side=TOP)
 
-long_label = tk.Label(root, text = "LON: " + str(weather.settings['lon']))
-long_label.pack(side=TOP)
+lon_label = tk.Label(root)
+lon_label.pack(side=TOP)
 
-temp_label = tk.Label(root, text = "Current Temperature: " + str(round(weather.current['temp'], 2)))
+temp_label = tk.Label(root)
 temp_label.pack(side=TOP)
 
-my_button = tk.Button(root, text="Update",command=weather.displayCurrent)
-my_button.pack(side=TOP)
+weather_conditons_label = tk.Label(root)
+weather_conditons_label.pack(side=TOP)
+
+#method that loads the weather data to the labels
+def loadWeatherData(lat, lon):
+    #update weather variable
+    weather = Weather(lat, lon)
+
+    location_label['text'] = "Location: " + str(weather.info['location'])
+    lat_label['text'] = "LAT: " + str(weather.settings['lat'])
+    lon_label['text'] = "LON: " + str(weather.settings['lon'])
+    temp_label['text'] = "Current Temperature: " + str(round(weather.current['temp'], 2))
+    weather_conditons_label['text'] = "Current Weather: " + weather.current['weather_conditions']
+
+loadWeatherData(lat, lon)
+
+#Update Button
+#TODO: API HAS A LIMITED NUMBER OF CALLS PER DAY, SO A LIMIT SHOULD BE ADDED OR THE PROGRAM SHOULD BE MADE TO ONLY AUTOMATICALLY UPDATE ~ EVERY MINUTE
+update_button = tk.Button(root, text="Update",command=lambda: loadWeatherData(lat, lon))
+update_button.pack(side=BOTTOM)
 
 root.mainloop()
